9c2c5b363a077d62681aaa1c4f483d1752b117f4
Jack Howard <jackhowaa@gmail.com>
2017-12-21 12:46:54 -0600

    Begin making dictionary for cleaner code solution

--- convert-html.js
+++ convert-html.js
@@ -1,12 +1,23 @@
 function convertHTML(str) {
-  // dumbfix 
+  
+    // dumbfix 
   var findAmpersands = / & /gi; // need space to prevent overlap 
   var findCarrots = /</gi;
   var findOtherCarrot = />/gi;
   var findQuote = /"/gi;
   var findApostrophe = /'/gi;
   
+  // need to store regex as a string 
+  // then use it as regex again 
+
+  // start making a dictionary to loop through 
+  let dictionary = {}; 
+  dictionary[String(findAmpersands)] = ' &amp; ';
   
+  console.log(dictionary);
+             
+  // new RegExp(pattern)
+  // via https://stackoverflow.com/questions/1695633/how-to-pass-a-variable-into-regex-in-jquery-javascript
   
   var ampersanded = str.replace(findAmpersands, ' &amp; ');
   var carroted = ampersanded.replace(findCarrots, '&lt;');
